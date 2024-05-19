698efe9d32d31c41d63dd26ddd99ea932f83172b
mufaddal lashkar <mufaddal.github@gmail.com>
2024-05-19 20:23:54 +0530

    delete getvideobyid slice

--- src/store/VideosSlice.js
+++ src/store/VideosSlice.js
@@ -28,19 +28,6 @@ export const getSearchVideos = createAsyncThunk(
     }
 )
 
-export const getVideoById = createAsyncThunk(
-    'videos/getVideoById',
-    async ({params}) => {
-        console.log(videoId);
-        const apiURL = `${server}/videos/v/:videoId`
-        const request = await axios.get(apiURL, {params})
-        .then((res) => {
-            return res
-        })
-        .catch((err) => console.log("Error while hitting api", err))
-    }
-)
-
 const VideosSlice = createSlice({
     name: "videos",
     initialState: {
@@ -61,21 +48,6 @@ const VideosSlice = createSlice({
             state.error = null;
         })
         .addCase(getVideos.rejected, (state, action) => {
-            state.loading = false;
-            state.videos = null;
-            state.error = action.error.message;
-        })
-        .addCase(getVideoById.pending, (state) => {
-            state.loading = true;
-            state.videos = null;
-            state.error = null;
-        })
-        .addCase(getVideoById.fulfilled, (state, action) => {
-            state.loading = false;
-            state.videos = action.payload;
-            state.error = null;
-        })
-        .addCase(getVideoById.rejected, (state, action) => {
             state.loading = false;
             state.videos = null;
             state.error = action.error.message;
